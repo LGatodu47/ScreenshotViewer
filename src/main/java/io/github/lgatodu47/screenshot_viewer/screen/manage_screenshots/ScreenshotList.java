c86aa91d83352d7154124b577e9da0ee0df7e881
LGatodu47 <62804774+LGatodu47@users.noreply.github.com>
2023-10-14 18:25:24 +0200

    ScreenshotViewer 1.2.1: support for jpeg images

--- src/main/java/io/github/lgatodu47/screenshot_viewer/screen/manage_screenshots/ScreenshotList.java
+++ src/main/java/io/github/lgatodu47/screenshot_viewer/screen/manage_screenshots/ScreenshotList.java
@@ -88,7 +88,7 @@ final class ScreenshotList extends AbstractParentElement implements Drawable, Se
 
             ScreenshotWidget.Context context = ScreenshotWidget.Context.create(() -> screenshotsPerRow, screenshotWidgets::indexOf);
             for (File file : files) {
-                if (file.isFile() && file.getName().endsWith(".png")) {
+                if (file.isFile() && (file.getName().endsWith(".png") || file.getName().endsWith(".jpg") || file.getName().endsWith(".jpeg"))) {
                     ScreenshotWidget widget = new ScreenshotWidget(mainScreen, childX, childY, childWidth, childHeight, context, file);
                     this.screenshotWidgets.add(widget);
                     this.elements.add(widget);
